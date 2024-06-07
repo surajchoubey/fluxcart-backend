74552baeedfb6d4e73ade7b10ec21185b70f1d02
surajchoubey <srjchoubey2@gmail.com>
2024-06-07 20:13:09 +0530

    GET -> POST

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { identify, createOrder, getOrders} = require('./controllers');
 
 // GET API to fetch messages
-router.get('/identify', identify);
+router.post('/identify', identify);
 
 // POST API to add a new message
 router.post('/orders', createOrder);
